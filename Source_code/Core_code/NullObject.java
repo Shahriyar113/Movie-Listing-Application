8172b5e2c2fd558f76be07aee20a176cf8aefbff
FAHIM <1805113@ugrad.cse.buet.ac.bd>
2024-05-09 05:32:30 +0600

    comment added

--- Source_code/Core_code/NullObject.java
+++ Source_code/Core_code/NullObject.java
@@ -2,6 +2,7 @@ package Source_code.Core_code;
 
 import java.util.ArrayList;
 
+//This "NullMovie" class is used to follow "null object pattern" to avoid null pointer exception
 class NullMovie extends Movie{
 
     public NullMovie() {
